4cb790c0b915d7433fb40aadc10a24e984a7ae08
Ludmila Berto <ludmilaberto5@gmail.com>
2024-08-27 14:18:15 -0300

    Inicialización del proyecto con Node.js, configuración de Express, EJS y archivos estáticos

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const app=express()
+
+
+// Configuro ejs como motor de plantilla
+app.set('view engine', 'ejs')
+
+// Configuracion de archivos estaticos(css, js, img)
+app.use(express.static('public'))
+
+// Ruta inicial
+app.get('/', (req,res)=>{
+    res.send('pagina inicial')
+})
+// Rutas estaticas
+
+
+// Rutas especificas
+
+
+
+// Configuración del servidor
+app.listen(4500, ()=>{
+    console.log('El servidor esta corriendo')
+})
