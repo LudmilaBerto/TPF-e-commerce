13ebeed2d2c5a910e045c44d25588f35b3d76f15
ybamBogado <bogadoivan52@gmail.com>
2024-09-04 15:52:32 -0300

    agregue roter, ccatalogo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app=express()
+// constante de rutas
+const tienda = require('./public/router/tienda');
 
 
 // Configuro ejs como motor de plantilla
@@ -8,18 +10,13 @@ app.set('view engine', 'ejs')
 // Configuracion de archivos estaticos(css, js, img)
 app.use(express.static('public'))
 
+
 // Ruta inicial
 app.get('/', (req,res)=>{
     res.render('index')
 })
 // Rutas estaticas
-
-
-// Rutas especificas
-app.get('/catalogo', (req,res)=>{
-    res.render('catalogo')
-})
-
+app.use('/tienda', tienda);
 
 // Configuración del servidor
 app.listen(4500, ()=>{
---
+++ public/router/tienda.js
@@ -0,0 +1,9 @@
+const express= require('express')
+const router=express.Router()
+
+router.get('/catalogo',(req,res)=>{
+    res.render('./catalogo')
+})
+
+module.exports = router;
+
