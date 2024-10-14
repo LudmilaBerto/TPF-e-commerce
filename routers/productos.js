ec215ffa9735fe006d65bfc02a8d9c6d591877b5
ybamBogado <bogadoivan52@gmail.com>
2024-10-14 18:06:00 -0300

    agregue filtos para habitaciones, tipo de muebles, tamaño y tipo de sofa

--- routers/productos.js
+++ routers/productos.js
@@ -1,10 +1,22 @@
 const express=require('express')
-const router=express.Router()
-const {getProductos, getProducto, getCarrito, postProducto, putProducto, deleteProducto} = require('../controllers/productos')
+const router=express.Router() 
+const {getProductos, getHabitaciones , getMuebles, getTamano ,getSofa, getProducto, getCarrito , postProducto, putProducto, deleteProducto} = require('../controllers/productos')
 
 // Ruta para obtener todos los productos(Shop) y crear un nuevo producto
 //  http://localhost:4500/tienda/
 router.route('/').get(getProductos).post(postProducto)
+
+//  http://localhost:4500/tienda/Habitacion/:categoria
+router.route('/habitacion/:categoria').get(getHabitaciones)
+
+//  http://localhost:4500/tienda/Mueble/:subCategoria
+router.route('/Mueble/:subCategoria').get(getMuebles)
+
+//  http://localhost:4500/tienda/Tamano/:tamano
+router.route('/Tamano/:tamano').get(getTamano)
+
+//  http://localhost:4500/tienda/TipoDeSofa/:sofa
+router.route('/TipoDeSofa/:sofa').get(getSofa)
 
 // Ruta para obtener, actualizar o eliminar un producto específico por ID
 // http://localhost:4500/tienda/productos/:id
